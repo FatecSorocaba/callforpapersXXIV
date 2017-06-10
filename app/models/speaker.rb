d21b93f2e972d0802eb9633066bb818c48254c8e
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-06-10 11:43:48 -0300

    Add model validations

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -3,5 +3,13 @@ class Speaker < ApplicationRecord
 
   accepts_nested_attributes_for :talk
 
+  validates_presence_of :talk, :name, :picture, :email, :biography
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
+                    length: { maximum: 140 }
+  validates_length_of :name, in: 4..100
+  validates_length_of :biography, maximum: 300
+  validates :website, format: { with: /\A(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\z/i },
+                      allow_blank: true
+
   mount_uploader :picture, PictureUploader
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,10 @@
 class Talk < ApplicationRecord
   belongs_to :speaker
-  
+
+  validates_presence_of :title, :description, :theme, :required_knowledge
+  validates_length_of :title, maximum: 100
+  validates_length_of :description, maximum: 300
+
   enum theme: [:web, :games, :dev_ops, :carrer, :design, :mobile, :analysis, :other]
   enum required_knowledge: [:beginner, :medium, :advanced]
 end
