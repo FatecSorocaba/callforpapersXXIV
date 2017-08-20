c10204914d23339b6c158c52d7344b55a153a9ea
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-08-20 00:23:16 -0300

    Update speaker_controller to use request domain

--- app/mailers/speaker_mailer.rb
+++ app/mailers/speaker_mailer.rb
@@ -1,9 +1,9 @@
 class SpeakerMailer < ApplicationMailer
   include Rails.application.routes.url_helpers
 
-  def submitted_talk(speaker)
+  def submitted_talk(speaker, domain)
     @speaker_name = speaker.name.split.first
-    @edit_link = edit_speaker_url(speaker, host: ENV['HOST_LINK'])
+    @edit_link = edit_speaker_url(speaker, host: domain)
     mail(to: speaker.email, subject: 'Obrigado por enviar sua proposta!')
   end
 end
