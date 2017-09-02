40e9c7d85725e614a096776a7142cd71642e2083
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-02 11:16:41 -0300

    Update InvitesController

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,5 +3,14 @@ class InvitesController < ApplicationController
   end
 
   def send_email
+    InvitesMailer.invite_to_talk(send_email_params[:name],
+                                 send_email_params[:email]).deliver_now
+
+    redirect_to new_invite_url, notice: 'Email enviado!'
+  end
+
+  private
+  def send_email_params
+    params.permit(:email, :name)
   end
 end
