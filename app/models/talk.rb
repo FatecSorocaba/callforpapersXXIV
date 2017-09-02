90733e752146c79559fb123ca3c464a36d48abeb
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-02 14:45:03 -0300

    Add kind to talk

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,10 +1,11 @@
 class Talk < ApplicationRecord
   belongs_to :speaker
 
-  validates_presence_of :title, :description, :theme, :required_knowledge
+  validates_presence_of :title, :description, :theme, :required_knowledge, :kind
   validates_length_of :title, maximum: 100
   validates_length_of :description, maximum: 300
 
   enum theme: [:web, :games, :dev_ops, :carrer, :design, :mobile, :analysis, :other]
   enum required_knowledge: [:beginner, :medium, :advanced]
+  enum kind: [:talk, :lightning, :course]
 end
