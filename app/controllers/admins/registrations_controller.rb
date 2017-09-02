287446598150582e0f2bbe25baf8cff09db84580
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-09-02 16:17:28 -0300

    Enable admin create another admin

---
+++ app/controllers/admins/registrations_controller.rb
@@ -0,0 +1,34 @@
+class Admins::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :set_minimum_password_length, only: [:new_admin]
+  before_action :redirect_unauthenticated, only: [:new_admin]
+
+  def new_admin
+    build_resource({})
+    yield resource if block_given?
+    respond_with resource
+  end
+
+  def create_admin
+    @admin = Admin.new new_admin_params
+
+    if @admin.save
+      redirect_to root_path
+    else
+      render new_admin @admin
+    end
+  end
+
+  def new
+    redirect_to root_path
+  end
+
+  private
+
+  def redirect_unauthenticated
+    redirect_to root_path unless admin_signed_in?
+  end
+
+  def new_admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
   root 'speakers#new'
 
-  resources :speakers, param: :uuid, only: [:new, :create, :edit, :update]
   resources :invites, only: :new do
     collection do
       post :send_email
     end
   end
+
+  devise_for :admins, controllers: {registrations: 'admins/registrations'}
+  namespace 'admins' do
+    devise_scope :admin do
+      get 'new_admin', to: 'registrations#new_admin'
+      post 'create_admin', to: 'registrations#create_admin'
+    end
+  end
+
+  resources :speakers, param: :uuid, only: [:index, :new, :create, :edit, :update]
 end
