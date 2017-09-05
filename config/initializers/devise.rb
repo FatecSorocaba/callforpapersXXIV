e76fb8241c4d33a72f5b125b827db9da4677674d
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-09-05 20:40:29 -0300

    Separate i18n yml into files

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'semanadetecnologiafatecso@noreply.com'
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
