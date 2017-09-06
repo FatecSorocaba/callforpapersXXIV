a6510466fb7e786f091db0a0e823e61136dcc208
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-09-06 10:28:02 -0300

    Place devise email to a env var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender = 'semanadetecnologiafatecso@noreply.com'
+  config.mailer_sender = ENV['DEVISE_EMAIL_HOST']
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
